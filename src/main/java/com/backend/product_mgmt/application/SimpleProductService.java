2c6594be79264e61bec9810d080bc4c79f0d991e
KangHyeonJu <khj010926@gmail.com>
2025-03-24 15:27:54 +0900

    상품 등록

--- src/main/java/com/backend/product_mgmt/application/SimpleProductService.java
+++ src/main/java/com/backend/product_mgmt/application/SimpleProductService.java
@@ -3,24 +3,27 @@ package com.backend.product_mgmt.application;
 import com.backend.product_mgmt.domain.Product;
 import com.backend.product_mgmt.infrastructure.ListProductRepository;
 import com.backend.product_mgmt.presentation.ProductDto;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SimpleProductService {
     private ListProductRepository listProductRepository;
+    private ModelMapper modelMapper;
 
     @Autowired
-    SimpleProductService(ListProductRepository listProductRepository){
+    SimpleProductService(ListProductRepository listProductRepository, ModelMapper modelMapper){
         this.listProductRepository = listProductRepository;
+        this.modelMapper = modelMapper;
     }
 
     public ProductDto add(ProductDto productDto){
-        Product product = ??;
+        Product product = modelMapper.map(productDto, Product.class);
 
         Product savedProduct = listProductRepository.add(product);
 
-        ProductDto savedProductDto = ??;
+        ProductDto savedProductDto = modelMapper.map(savedProduct, ProductDto.class);
 
         return savedProductDto;
     }
